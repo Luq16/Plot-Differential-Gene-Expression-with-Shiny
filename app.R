b032355e62de7c7fd5a0e80aa1c53fe7ef357705
Luq16 <luqawoniyi@gmail.com>
2021-11-06 14:17:40 +0000

    Complete server code

--- app.R
+++ app.R
@@ -176,6 +176,73 @@ server <- function(input, output) {
         content = function(file){
             write.csv(read.csv(full_file_name()), quote = FALSE,file)
         } )
+    output$myplot = renderPlot({
+        g_x <- read.csv(full_file_name())
+        
+        p <- ggplot(g_x, aes(x=Gene_ID, y=log(Relative_expression_levels),
+                             fill=Resistant_or_Susceptible_strains)) +
+            
+            geom_bar(stat="identity", position=position_dodge()) +
+            geom_errorbar(aes(ymin=log(Relative_expression_levels)-(SD/10),
+                              ymax=log(Relative_expression_levels)+(SD/10)),width=.3,
+                          position=position_dodge(.9))
+        p + scale_fill_brewer(palette="Paired")+
+            ggtitle(paste("Relative expression levels of candidate gene list","\n",
+                          "expressed as mean fold difference between pre- and",
+                          "\n", "post-infection ± standard deviation (SD) ")) +
+            guides(fill=guide_legend(title=NULL))
+        
+        p$theme <- theme(axis.text.x = element_text(angle = 90, hjust = 1))
+        p$labels$x <- "Gene ID"
+        p$labels$y <- "Log (base 10) Relative Expression Levels"
+        p$labels$fill <- NULL
+        
+        return(p)
+        
+    })
+    
+    
+    # renderDT() from DT library is a replacement for Shiny renderDataTable()
+    output$datatable1 <- renderDT(datatable(g1))
+    output$datatable2 <- renderDT(datatable(g2))
+    output$datatable3 <- renderDT(datatable(g3))
+    
+    output$text1 <- renderUI({
+        if(input$More_info=="Introduction"){
+            includeHTML("inc/introduction.html")
+        } else if(input$More_info=="Information"){
+            includeHTML("inc/information.html")
+        } else if(input$More_info=="Help"){
+            includeHTML("inc/help.html")
+        } else if(input$More_info=="Table-1"){
+            DTOutput('datatable1')
+        } else if(input$More_info=="Table-2"){
+            DTOutput('datatable2')
+        } else if(input$More_info=="Table-3"){
+            DTOutput('datatable3')
+        } else if(input$More_info=="References"){
+            includeHTML("inc/references.html")
+        }
+    })
+    
+    output$messageMenu <- renderMenu({
+        # Code to generate each of the messageItems here, in a list. This assumes
+        # that gn is a data frame with two columns, 'GeneID' and 'Gene'.
+        messageList <- apply(gn[1:3, ], 1, function(row) {
+            messageItem(
+                from = row[["GeneID"]],
+                message = row[["Gene"]],
+                icon = icon("question"),
+                href = paste("https://www.genecards.org/cgi-bin/carddisp.pl?gene=",
+                             as.character(row[["GeneID"]]),
+                             sep = '')
+            )
+        })
+
+        # This is equivalent to calling:
+        #   dropdownMenu(type="messages", msgs[[1]], msgs[[2]], ...)
+        dropdownMenu(type = "messages", .list = messageList)
+    })
     
 }
 # ===================================================== server.R END ============
